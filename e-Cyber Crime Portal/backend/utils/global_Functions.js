60a5ba1c84bd1e8377499340e6fc3b9da171082d
13-Bhupendra <bhupendrakp2008@gmail.com>
2026-01-12 22:25:59 +0530

    12/01/2026

--- e-Cyber Crime Portal/backend/utils/global_Functions.js
+++ e-Cyber Crime Portal/backend/utils/global_Functions.js
@@ -1,13 +1,13 @@
 // const latterToNumber = (name)=>{
 //     const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//     const code = "";
+//     let code = "";
 
 //     const firstThreeChar = name.substring(0 , 3).toUpperCase();
 
-//         for(let i = 0;  i < firstThreeChar.lenght; i++){
+//         for(let i = 0;  i < firstThreeChar.length; i++){
 //             const char = firstThreeChar[i];
 //             const index = alphabet.indexOf(char) + 1 
-//             code += index.toString().padStart(2 , "0");
+//             code += index.toString();
 //         }
 //         return console.log(code);
 //     }
